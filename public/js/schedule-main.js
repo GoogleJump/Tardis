3799c6edba53f30431e38dfef38db0b0d6a1e10a
Evan <forbes.evan@gmail.com>
2014-07-12 15:34:26 -0500

    Selected courses now added to table, can be removed

--- public/js/schedule-main.js
+++ public/js/schedule-main.js
@@ -1,4 +1,8 @@
+var selectedCourses = [];
+
 $(function () {
+   $("#selected-courses").hide();
+
   $("#course_input").autocomplete({
       source: function (request, response) {
          console.log("requesting: "+request);
@@ -11,7 +15,7 @@ $(function () {
               console.log("response received: "+data+" status: "+status);
                response($.map(data, function (el) {
                   return {
-                     label: el.number,
+                     number: el.number,
                      id: el._id,
                      name: el.name
                   };
@@ -28,20 +32,46 @@ $(function () {
          
          // set an onFocus event to show the result on input field when result is focused
          focus: function (event, ui) { 
-            this.value = ui.item.label; 
+            this.value = ui.item.number; 
             console.log("focused: "+ui.item);
             // Prevent other event from not being execute
             event.preventDefault();
          },
          select: function (event, ui) {
             // Prevent value from being put in the input:
-            this.value = ui.item.label;
+            this.value = "";
             
+            selectedCourses.push(ui.item);
+            displaySelectedCourses();
             console.log("selected: "+ui.item.id);
             // Prevent other event from not being execute            
             event.preventDefault();
             // optionnal: submit the form after field has been filled up
             //$('#quicksearch').submit();
          }
-  });
+  })
+   .autocomplete( "instance" )._renderItem = function( ul, item ) {
+      return $( "<li>" )
+        .append( "<a>" + item.number + "<br>" + item.name + "</a>" )
+        .appendTo( ul );
+    };;
 });
+
+function displaySelectedCourses() {
+   if(selectedCourses.length==0) {
+      $("#none-selected").show();
+      $("#selected-courses").hide();
+   } else {
+      $("#none-selected").hide();
+      $("#selected-courses").find("tr:gt(0)").remove(); //remove all existing rows except title
+      for(var index in selectedCourses) {
+         $("#selected-courses tr:last").after("<tr><td>"+selectedCourses[index].number+"</td><td>"+selectedCourses[index].name+"</td><td onclick=\"removeCourse("+index+");\"><a href=\"#\">Remove</a></td></tr>");
+      }
+      $("#selected-courses").show();
+   }
+}
+
+function removeCourse(index) {
+   selectedCourses.splice(index, 1);
+   displaySelectedCourses();
+}
