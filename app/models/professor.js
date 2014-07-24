2e21d2399d2f5e4ad1e564034b762937f747b174
Evan Forbes <forbes.evan@gmail.com>
2014-07-24 17:18:19 -0500

    Added average rating and percent recommending

--- app/models/professor.js
+++ app/models/professor.js
@@ -9,4 +9,23 @@ var professorSchema = mongoose.Schema({
     _raters:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}]
 });
 
+professorSchema.methods.getAverageRating = function() {
+    var sum=0;
+    for(var i=0;i<this._ratings.length;i++) {
+    	sum+=this._ratings[i].overall;
+    }
+    return sum/this._ratings.length;
+};
+
+professorSchema.methods.getRecommendPercent = function() {
+    var sum=0;
+    for(var i=0;i<this._ratings.length;i++) {
+    	if(this._ratings[i].recommend){
+    		sum++;
+    	}
+    }
+    var dec = sum/this._ratings.length;
+    return parseFloat(dec*100).toFixed(0)+'%';
+};
+
 module.exports = mongoose.model('Professor', professorSchema);
