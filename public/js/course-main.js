8d39bd2c0cc1c33202a4f661a815603399551f70
Evan Forbes <forbes.evan@gmail.com>
2014-08-03 22:12:09 -0700

    Can add course to schedule from /course

---
+++ public/js/course-main.js
@@ -0,0 +1,26 @@
+
+$(function () {
+	$("#add-to-schedule-button").click(addToSchedule);
+})
+
+function addToSchedule() {
+	var courseId= $("#courseId").val();
+	console.log("adding "+courseId);
+	$.ajax({
+    url: "/schedule/add-course",
+    type: "POST",
+    data: {courseId:courseId}, 
+    success: function (data, status) {
+      if(data.error) {
+        console.log(data.error);
+        //todo: show error message
+      } else {
+      	console.log("add success");
+        //todo: show success message
+      }
+    },
+    error: function(xhr,status,error){
+       //todo: show error message
+    }
+  });
+}
