46aa5cb3c790af0ff970601f36c2e990d41a933c
Evan Forbes <forbes.evan@gmail.com>
2014-08-08 17:31:21 -0500

    db config change for heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://evanforbes:Asimple123@kahana.mongohq.com:10067/app28283367' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
